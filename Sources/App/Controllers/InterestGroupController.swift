145ac8fdcc1456a6c346dfa0bb5b7aef98e0a44e
Michael Critz <github@michaelcritz.com>
2022-12-19 22:32:57 -0800

    Admin check for create / delete interest group

--- Sources/App/Controllers/InterestGroupController.swift
+++ Sources/App/Controllers/InterestGroupController.swift
@@ -16,12 +16,20 @@ struct InterestGroupController: RouteCollection {
     }
 
     func create(req: Request) async throws -> InterestGroup {
+        guard try await req.isAdmin() else {
+            req.logger.info("Failed Group create \(req)")
+            throw Abort(.unauthorized)
+        }
         let group = try req.content.decode(InterestGroup.self)
         try await group.save(on: req.db)
         return group
     }
 
     func delete(req: Request) async throws -> HTTPStatus {
+        guard try await req.isAdmin() else {
+            req.logger.info("Failed Group delete \(req)")
+            throw Abort(.unauthorized)
+        }
         guard let group = try await InterestGroup.find(req.parameters.get("groupID"), on: req.db) else {
             throw Abort(.notFound)
         }
