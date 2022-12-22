14664fd9fe6706e9f7381174f7d10f99350fb07c
Michael Critz <github@michaelcritz.com>
2022-12-22 12:20:35 -0800

    Improve Configure.swift. Improve env_example

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,8 +6,8 @@ import FluentSQLiteDriver
 import Vapor
 import JWT
 
-fileprivate enum ConfigureError: String, Error {
-    case environmentNotSet = "No JWT_SIGNING_SECRET set in the environment"
+fileprivate enum ConfigureError: Error {
+    case environmentNotSet(reason: String)
 }
 
 // configures your application
@@ -15,7 +15,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    configureDatabase(on: app)
+    try configureDatabase(on: app)
     try databaseMigrations(on: app)
     
     try configureJWT(on: app)
@@ -23,13 +23,11 @@ public func configure(_ app: Application) throws {
     try routes(app)
 }
 
-func configureDatabase(on app: Application) {
+func configureDatabase(on app: Application) throws {
     if app.environment.name != Environment.testing.name {
         guard let username = Environment.get("DATABASE_USERNAME"),
               let database = Environment.get("DATABASE_NAME") else {
-            preconditionFailure("""
-            Environment values are not set
-            """)
+            throw ConfigureError.environmentNotSet(reason: "Database configuration not set in environment")
         }
         app.databases.use(.postgres(
             hostname: Environment.get("DATABASE_HOST") ?? "localhost",
@@ -38,7 +36,7 @@ func configureDatabase(on app: Application) {
             password: Environment.get("DATABASE_PASSWORD") ?? "",
             database: database
         ), as: .psql)
-    } else {
+    } else { // Environment is testing. Use in-memory sqlite.
         app.databases.use(.sqlite(.memory), as: .sqlite)
     }
 }
@@ -53,6 +51,7 @@ func databaseMigrations(on app: Application) throws {
     app.migrations.add(CreateVenue())
     app.migrations.add(CreateEvent())
     
+    // Always automigrate dev/test
     #if DEBUG
     print("DEBUG: Automigrate Start")
     try app.autoMigrate().wait()
@@ -62,7 +61,7 @@ func databaseMigrations(on app: Application) throws {
 
 func configureJWT(on app: Application) throws {
     guard let secret = Environment.get("JWT_SIGNING_SECRET") else {
-        throw ConfigureError.environmentNotSet
+        throw ConfigureError.environmentNotSet(reason: "NO JWT_SIGNING_SECRET set in envirnoment")
     }
     app.jwt.signers.use(.hs256(key: secret))
 }
